574461309ae2a99f043e39a187863322c229985c
Nikita Guchenko <guchenko.n@gmail.com>
2017-09-02 22:11:56 +0300

    Completed input and output functions

--- 2-InputOutput/src/Client.java
+++ 2-InputOutput/src/Client.java
@@ -8,12 +8,16 @@ public class Client {
         int age;
 
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name:  ");
         name = input.nextLine();
+        System.out.print("Enter your city:  ");
         city = input.nextLine();
+        System.out.print("Enter your hobby: ");
         hobby = input.nextLine();
+        System.out.print("Enter your age:   ");
         age = input.nextInt();
 
-
+        System.out.println("------------------");
         Printer.out1(name, city, age, hobby);
         System.out.println("------------------");
         Printer.out2(name, city, age, hobby);
