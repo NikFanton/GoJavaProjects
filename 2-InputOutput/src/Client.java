a452d384ff8f42a2d452967b007c0f700aba4d7c
Nikita Guchenko <guchenko.n@gmail.com>
2017-09-02 20:55:58 +0300

    Input information

--- 2-InputOutput/src/Client.java
+++ 2-InputOutput/src/Client.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         String name;
         String city;
         String hobby;
         int age;
+
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        city = input.nextLine();
+        age = input.nextInt();
+        hobby = input.nextLine();
         
     }
 }
