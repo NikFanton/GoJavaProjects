643c215752b7c5442d20de3e97f0b1ba75ded476
Nikita Guchenko <guchenko.n@gmail.com>
2017-09-04 19:46:34 +0300

    Created function drawTree();

---
+++ off-1/src/draw/Tree.java
@@ -0,0 +1,21 @@
+package draw;
+
+public class Tree {
+
+    public static void drawTree() {
+        System.out.println(  "        /\\ \n" +
+                             "       /  \\ "+"\n" +
+                             "      / || \\ "+"\n" +
+                             "     /  ||  \\ "+"\n" +
+                             "    /   ||   \\ "+"\n" +
+                             "   /    ||    \\ "+"\n" +
+                             "  /     ||     \\ "+"\n" +
+                             "        ||     \n " +
+                             " ---------------");
+
+    }
+
+    public static void main(String[] args) {
+        drawTree();
+    }
+}
