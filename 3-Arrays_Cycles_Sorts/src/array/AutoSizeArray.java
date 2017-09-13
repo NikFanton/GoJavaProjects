502fd7364ec4d708e9a7555ca87eef6321a8e102
Nikita Guchenko <guchenko.n@gmail.com>
2017-09-13 21:20:43 +0300

    Completed third task

--- 3-Arrays_Cycles_Sorts/src/array/AutoSizeArray.java
+++ 3-Arrays_Cycles_Sorts/src/array/AutoSizeArray.java
@@ -9,11 +9,38 @@ public class AutoSizeArray {
     AutoSizeArray() {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        System.out.println(str);
-
+        int begin = 0;
+        int end = 0;
+        int size = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                end = i;
+                size++;
+                begin = end + 1;
+            } else if (i == str.length() - 1) {
+                end = i + 1;
+                size++;
+            }
+        }
+        begin = 0;
+        end = 0;
+        array = new Array(size);
+        for (int i = 0, j = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                end = i;
+                array.setElement(Double.parseDouble(str.substring(begin, end)), j);
+                j++;
+                begin = end + 1;
+            } else if (i == str.length() - 1) {
+                end = i + 1;
+                array.setElement(Double.parseDouble(str.substring(begin, end)), j);
+                j++;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        
+        AutoSizeArray asArray = new AutoSizeArray();
+        asArray.array.show();
     }
 }
