840c33f71a4a427dd25317ed46b0a01db50a972d
Nikita Guchenko <guchenko.n@gmail.com>
2017-09-23 13:00:44 +0300

    Completed tasks 6-7

--- 4-Function/src/Functions.java
+++ 4-Function/src/Functions.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Functions {
 
-    public static void task1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("X = ");
-        int x = scanner.nextInt();
+    public static void task1(int x) {
         for (int i = 1; i <= x; i++) {
             System.out.println(i);
         }
@@ -46,10 +43,7 @@ public class Functions {
         return (a > b) ? a : b;
     }
 
-    public static void task5() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("X = ");
-        int x = scanner.nextInt();
+    public static void task5(int x) {
         task5(1, x);
     }
 
@@ -59,11 +53,79 @@ public class Functions {
         task5(++i, x);
     }
 
+    public static void task6(int height, int width) {
+        task6(height, width, 0, 0);
+    }
+
+    public static void task6(int height, int width, int i, int j) {
+        if (j >= width) {
+            System.out.println();
+            j = 0;
+            i++;
+        }
+
+        if (i >= height) {
+            return;
+        }
+        j++;
+        System.out.print("+");
+        task6(height, width, i, j);
+    }
+
+    public static void task7() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        int flag;
+        do {
+            do {
+                System.out.print("Which task do you want to run (1-6): ");
+                choice = scanner.nextInt();
+            } while (choice < 1 || choice > 6);
+
+            switch (choice) {
+                case 1:
+                    System.out.print("X = ");
+                    int x = scanner.nextInt();
+                    task1(x);
+                    break;
+                case 2:
+                    System.out.print("Height = ");
+                    int height = scanner.nextInt();
+                    System.out.print("Width = ");
+                    int width = scanner.nextInt();
+                    drawRectangle(height, width);
+                    break;
+                case 3:
+                    System.out.print("Size = ");
+                    int size = scanner.nextInt();
+                    drawRectangle(size);
+                    break;
+                case 4:
+                    System.out.print("A = ");
+                    float a = scanner.nextFloat();
+                    System.out.print("B = ");
+                    float b = scanner.nextFloat();
+                    System.out.println("max: " + getMax(a, b));
+                    break;
+                case 5:
+                    System.out.print("X = ");
+                    int x_recursion = scanner.nextInt();
+                    task5(x_recursion);
+                    break;
+                case 6:
+                    System.out.print("Height = ");
+                    int heightRecursion = scanner.nextInt();
+                    System.out.print("Width = ");
+                    int widthRecursion = scanner.nextInt();
+                    task6(heightRecursion, widthRecursion);
+                    break;
+            }
+            System.out.print("Would you like to continue(1 - yes): ");
+            flag = scanner.nextInt();
+        } while (flag == 1);
+    }
+
     public static void main(String[] args) {
-//        task1();
-//        drawRectangle(3, 4);
-//        drawRectangle(3);
-//        System.out.println(getMax(2, (float) 2.9));
-        task5();
+        task7();
     }
 }
