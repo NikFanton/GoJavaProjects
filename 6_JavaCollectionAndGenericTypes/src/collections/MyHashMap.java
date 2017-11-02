0f1702087b4769e39df769ed606a74b570759d4f
Nikita Guchenko <guchenko.n@gmail.com>
2017-11-02 17:01:36 +0200

    Create new version of HashMap

--- 6_JavaCollectionAndGenericTypes/src/collections/MyHashMap.java
+++ 6_JavaCollectionAndGenericTypes/src/collections/MyHashMap.java
@@ -1,99 +1,88 @@
 package collections;
 
-public class MyHashMap<K, V> {
+public class MyHashMap<Key, Value> {
     private int size = 0;
-    private Node node;
+    private int M = 16;
+    private Key[] keys;
+    private Value[] values;
 
-    public class Node<K, V> {
-        K key;
-        V value;
-        Node <K, V> prev;
-
-        Node(K key , V value , Node <K, V> prev) {
-            this.value = value;
-            this.key = key;
-            this.prev = prev;
-        }
+    public MyHashMap() {
+        keys = (Key[]) new Object[M];
+        values = (Value[]) new Object[M];
     }
 
-    public void put(K key , V value) {
-        if (node == null) {
-            node = new Node<> ( key , value , null );
-            size++;
-        } else {
-            Node<K, V> valueNode = node;
-            boolean unique = true;
-            do {
-                if (valueNode.key.equals(key)) {
-                    valueNode.value = value;
-                    unique = false;
-                }
-                valueNode = valueNode.prev;
-            } while (valueNode != null && unique);
-            if (unique) {
-                this.node = new Node<K, V> ( key , value , this.node );
-                size++;
+    public MyHashMap(int capacity) {
+        M = capacity;
+        size = 0;
+        keys = (Key[]) new Object[M];
+        values = (Value[]) new Object[M];
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    private void resize(int capacity) {
+        MyHashMap<Key, Value> temp = new MyHashMap<Key, Value>(capacity);
+        for (int i = 0; i < M; i++) {
+            if (keys[i] != null) {
+                temp.put(keys[i], values[i]);
             }
         }
+        keys = temp.keys;
+        values = temp.values;
+        M = temp.M;
     }
 
-    public void clear() {
-        for (Node <K, V> n = node ; n != null ;) {
-            Node <K, V> next = n.prev;
-            n.key = null;
-            n.value = null;
-            n = next;
+    public void put(Key key, Value value) {
+        if (size >= M/2) { resize(2 * M); }
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
         }
-        node = null;
-        size = 0;
+        keys[i] = key;
+        values[i] = value;
+        size++;
     }
 
     public int size() {
         return size;
     }
 
-    public V get( K key ) {
-        Node <K, V> valueNode = node;
-        if ( valueNode == null ) return null;
-        while ( ! valueNode.key.equals ( key ) ) {
-            if ( valueNode.prev == null ) return null;
-            valueNode = valueNode.prev;
-        }
-        return valueNode.value;
-    }
-
-    public void remove(K key) {
-        int count = 0;
-        Node<K, V> iterator = node;
-        Node<K, V> Iterator1 = node;
-        do {
-            if (iterator.key.equals(key) && iterator.prev == null) {
-                Iterator1.prev = null;
-                size--;
-                break;
-            } else if (iterator.key.equals(key)) {
-                iterator.key = iterator.prev.key;
-                iterator.value = iterator.prev.value;
-                iterator.prev = iterator.prev.prev;
-                size--;
-                break;
+    public Value get(Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                return values[i];
             }
-            Iterator1 = iterator;
-            iterator = iterator.prev;
-            count++;
-
-        } while (size != count);
-    }
-    public void show() {
-        Node<K, V> valueNode = node;
-        if (valueNode == null) {
-            System.out.println("The hash map is empty");
-            return;
         }
-        do {
-            System.out.println(valueNode.key + ":" + valueNode.value);
-            valueNode = valueNode.prev;
-        } while (valueNode != null);
+        return null;
+    }
+
+    public void delete(Key key) {
+        if (get(key) == null) { return; }
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % M;
+        }
+        keys[i] = null;
+        values[i] = null;
+        i = (i + 1) % M;
+        while (keys[i] != null) {
+            Key keyToRedo = keys[i];
+            Value valueToRedo = values[i];
+            keys[i] = null;
+            values[i] = null;
+            size--;
+            put(keyToRedo, valueToRedo);
+            i = (i + 1) % M;
+        }
+        size--;
+        if (size > 0 && size == M/8) {
+            resize(M/2);
+        }
     }
 
     public static void main(String[] args) {
@@ -102,6 +91,6 @@ public class MyHashMap<K, V> {
         hashMap.put("2", "two");
         hashMap.put("10", "ten");
         hashMap.put("10", "tennn");
-        hashMap.show();
+        System.out.println(hashMap.get("10"));
     }
 }
